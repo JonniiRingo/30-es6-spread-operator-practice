5ee15feec81cc11935a23a09bd0e02101eef95e7
Jonnii Ringo <jonathansamuels@Jonathans-MBP-2.lan>
2025-01-04 20:58:02 -0800

    Finished challenge

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,51 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
+
+  // Set up state variable for the text;
+  const [inputText, setInputText] = useState(" ");
+  // Set up state variable fot the array of content to be mapped and rendered. Initialized to an empty array. 
+  const [items, setItems] = useState([]);
+
+  // set up the function that performs the feature
+  function handleChange(event){
+    //Tap into the event value
+    const newValue = event.target.value
+    setInputText(newValue)
+  }
+
+  //Set up function to handle the push to the list
+  function addItem(){
+      setItems((prevItems) => {
+        return [...prevItems, inputText]
+  });
+  setInputText(" "); 
+
+}
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+
+        <input 
+          type="text" 
+          value={inputText}
+          onChange={handleChange}
+          />
+
+        <button onClick={addItem}>
           <span>Add</span>
         </button>
       </div>
+
+
       <div>
-        <ul>
-          <li>A Item</li>
+        <ul> {items.map((toDoItem) =>{
+                return <li>{toDoItem}</li>;})}
+
         </ul>
       </div>
     </div>
